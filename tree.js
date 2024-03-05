b71f57456f6a96a60a22fd6920b3be35231ebe9e
Conor Kay <conorkay1@gmail.com>
2024-03-04 20:30:37 -0500

    Work

--- tree.js
+++ tree.js
@@ -8,13 +8,40 @@ class Node {
 
 class Tree {
   constructor(array) {
-    this.root = this.buildTree(mergeSort(array));
+    this.array = removeDupes(mergeSort(array));
+    this.root = this.buildTree(this.array, 0, this.array.length - 1);
   }
 
-  buildTree(array) {
-    let mid = Math.floor(array.length / 2);
+  buildTree(array, start, end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (start > end) {
+      return null;
+    }
+
     let root = new Node(array[mid]);
+    if (array.length === 0) {
+      return null;
+    }
+
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+
+    return root;
   }
+
+  insert(value){
+    let newNode = new Node(value);
+  }
+
+  findValue(value) {
+    let currNode = this.root;
+    if (currNode.data === value) {
+      return currNode;
+    }
+  }
+
+  height(node) {}
 }
 
 function merge(left, right) {
@@ -42,3 +69,34 @@ function mergeSort(array) {
 
   return merge(left, right);
 }
+
+function removeDupes(arr) {
+  var i = 0;
+  while (i < arr.length) {
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i + 1, 1);
+    } else {
+      ++i;
+    }
+  }
+  return arr;
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+let array = [5, 3, 6, 7, 2, 89, 77, 43, 32, 5, 5];
+let tree = new Tree(array);
+console.log(array);
+console.log(removeDupes(mergeSort(array)));
+prettyPrint(tree.root);
